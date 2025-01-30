04f9aac76bdb6f294c06c183826dfecaaf044294
Dominguez29 <alondradom05@gmail.com>
2025-01-30 12:15:21 -0600

    Update Practica Victor.py

--- Practica Victor.py
+++ Practica Victor.py
@@ -23,6 +23,41 @@ def division():
         num2 = float(input("Ingrese el denominador: "))
         if num2 == 0:
             print("La división por cero no está permitida.")
+else:
+print(f"El resultado de la division es:{num1 / num2}")
+except ValueError:
+print("Por favor, ingrese valores numéricos válidos.")
+
+def factorial():
+    try:
+        num = int(input("ingrese un numero para calcular su factorial:"))
+        if num < 0:
+            print("El factorial no esta definido para numeros negativos:")
+        else:
+            resultado = math.factorial(num)
+            print(f"El factorial de {num} es: {resultado}")
+    except ValueError:
+        print("Por favor, ingrese un numero valido.")
+ 
+def tabla_multiplicar():
+    try:
+        num = int(input("Seleccione la tabla de multiplicar (1 al 10):"))
+        if 1 <= num <=10:
+            for i in range(1,11):
+                print(f"{num} x {i} = {num * i}")
+        else:
+            print("Por favor, seleccione un numero entre 1 y 10.")
+    except ValueError:
+        print("Por favor, ingrese un numero entero valido.")
+
+def calcular_potencia():
+    try:
+        opcion = int(input("Seleccione: 1 para cuadrado, 2 para cubo."))
+        if opcion in [1,2]:
+            num = float(input("Ingrese el numero:"))
+            exponente = 2 if opcion == 1 else 3
+            print(f"El resultado de {num}^{exponente} es: {math.pow(num,exponente)}")
+            
      
       
   
